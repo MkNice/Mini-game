92244272ce391c4d0558f7523d628b162b6f6220
MkIINice <Mk1Hitch@gmail.com>
2022-12-25 12:41:00 +0200

    add game-modal-dialog

---
+++ src/app/welcome-game-modal/welcome-game-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-game-modal',
+  templateUrl: './welcome-game-modal.component.html',
+  styleUrls: ['./welcome-game-modal.component.css']
+})
+export class WelcomeGameModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
