74be7fe11ca9703926cdc2a46be543f58f8354ed
MkIINice <Mk1Hitch@gmail.com>
2022-12-25 13:17:03 +0200

    add main functionality, styles need to be modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,28 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { GameComponent } from './game/game.component';
-import { WelcomeGameModalComponent } from './welcome-game-modal/welcome-game-modal.component';
+import { GameComponent } from './components/game/game.component';
+import { WelcomeGameModalComponent } from './modalDialogs/welcome-game-modal/welcome-game-modal.component';
+import { ResultsGameModalComponent } from './modalDialogs/results-game-modal/results-game-modal.component';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
     GameComponent,
     WelcomeGameModalComponent,
+    ResultsGameModalComponent,
   ],
-  exports: [
-    MatDialogModule,
-  ],
+
   imports: [
     BrowserModule,
+    MatDialogModule,
     BrowserAnimationsModule,
-
+    FormsModule,
+    CommonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
